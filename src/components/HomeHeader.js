aa83ea4dd5d854048f157173b443fb9614d197b4
Julia Szczerbowska <julia.szczerbowska@gmail.com>
2019-12-05 18:01:42 +0100

    I added header image and positioned it, added React Router Link for login and registration as well.

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import headerPhoto from '../assets/Home-Hero-Image.jpg';
 
 class HomeHeader extends Component {
 	render() {
-		return <p>Home header</p>;
+		return (
+			<header>
+				<div className="photo">
+					<img className="photo__img" src={headerPhoto} alt="zdjecie kartonu z rzeczami" />
+				</div>
+				<div className="menu">
+					<nav className="menu__nav">
+						<Link to="/logowanie">Zaloguj</Link>
+						<Link to="/rejestracja">Załóż konto</Link>
+					</nav>
+				</div>
+			</header>
+		);
 	}
 }
 
