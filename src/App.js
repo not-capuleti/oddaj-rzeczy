a31bc84a1022d0551f66ef33cba7a0ec4115ed04
Julia Szczerbowska <julia.szczerbowska@gmail.com>
2019-12-08 20:36:34 +0100

    Changed Normalize to Reset.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './components/Home';
+import Home from './components/Home/Home';
 
 function App() {
 	return (
--- src/components/Home/HomeHeader.js
+++ src/components/Home/HomeHeader.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import headerPhoto from '../assets/Home-Hero-Image.jpg';
+import headerPhoto from '../../assets/Home-Hero-Image.jpg';
 
 class HomeHeader extends Component {
 	render() {
 		return (
 			<header>
 				<div className="photo">
-					<img className="photo__img" src={headerPhoto} alt="zdjecie kartonu z rzeczami" />
+					<img className="photo__img" src={headerPhoto} alt="" />
 				</div>
 				<div className="menu">
 					<nav className="menu__nav">
