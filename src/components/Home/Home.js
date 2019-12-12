bc6549c8940c18afacf43a3d11fcaee9f5a55eb7
Julia Szczerbowska <julia.szczerbowska@gmail.com>
2019-12-13 00:34:32 +0100

    Icons component created. Basic style added to position icons with text.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react';
 import Header from './Header/Header';
 import ThreeColumns from './ThreeColumns/ThreeColumns';
+import SimpleSteps from './SimpleSteps/SimpleSteps';
 
 const Home = () => {
 	return (
 		<Fragment>
 			<Header />
 			<ThreeColumns />
+			<SimpleSteps />
 		</Fragment>
 	);
 };
---
+++ src/components/Home/SimpleSteps/SimpleSteps.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Decoration from '../svgComponents/Decoration';
+import Icons from '../SimpleSteps/Icons';
+
+const SimpleSteps = () => {
+	return (
+		<section className="simple-steps">
+			<h2>Wystarczą 4 proste kroki</h2>
+			<Decoration />
+			<Icons />
+		</section>
+	);
+};
+
+export default SimpleSteps;
