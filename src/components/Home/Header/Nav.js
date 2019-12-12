6ba988f19604eb8ca6aae0576950ce2e9a65d633
Julia Szczerbowska <julia.szczerbowska@gmail.com>
2019-12-12 16:30:51 +0100

    Changed Nav component: now generating its Links using Map.

--- src/components/Home/Header/Nav.js
+++ src/components/Home/Header/Nav.js
@@ -3,23 +3,21 @@ import { Link } from 'react-scroll';
 
 class Nav extends Component {
 	render() {
+		const navElements = [
+			{ title: 'Start', to: '' },
+			{ title: 'O co chodzi?', to: '' },
+			{ title: 'O nas', to: '' },
+			{ title: 'Fundacja i organizacje', to: '' },
+			{ title: 'Kontakt', to: '' }
+		];
+
 		return (
 			<nav className="header__menu__nav">
-				<Link to="" spy={true} smooth={true} offset={0} duration={500}>
-					Start
-				</Link>
-				<Link to="" spy={true} smooth={true} offset={0} duration={500}>
-					O co chodzi?
-				</Link>
-				<Link to="" spy={true} smooth={true} offset={0} duration={500}>
-					O nas
-				</Link>
-				<Link to="" spy={true} smooth={true} offset={0} duration={500}>
-					Fundacja i organizacje
-				</Link>
-				<Link to="" spy={true} smooth={true} offset={0} duration={500}>
-					Kontakt
-				</Link>
+				{navElements.map((element) => (
+					<Link to={element.to} spy={true} smooth={true} offset={0} duration={500}>
+						{element.title}
+					</Link>
+				))}
 			</nav>
 		);
 	}
